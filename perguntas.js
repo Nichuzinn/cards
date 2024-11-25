54582880d8fcdd01269c1983446d45d43752e2c5
Nicolas <167724252+Nichuzinn@users.noreply.github.com>
2024-11-25 11:06:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'Raiz de 4?',
+    '2'
+)
+
+criaCartao(
+    'Matematica',
+    '3+4-1?',
+    '6'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é Relevo?',
+    'conjunto de formas que se observam na superfície da Terra, sendo o resultado da ação de diversos fatores internos e externos'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz cafe em Inglês?',
+    'Cafe em ingles é coffe'
+)
